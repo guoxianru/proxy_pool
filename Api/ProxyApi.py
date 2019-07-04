72a2b122155c3613778dbe325f2c7b6d5024db76
guoxianru <820685755@qq.com>
2019-07-04 15:29:18 +0800

    添加随机获取User-Agent功能
    Signed-off-by: guoxianru <820685755@qq.com>

--- Api/ProxyApi.py
+++ Api/ProxyApi.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from werkzeug.wrappers import Response
 from flask import Flask, jsonify, request
 
@@ -27,6 +28,7 @@ api_list = {
     'get_all': u'获取所有代理IP',
     'get_status': u'代理池当前状态',
     'refresh': u'刷新代理池(refresh会有守护程序定时执行，由api直接调用性能较差，不建议使用)',
+    'user_agent': '获取一个USER_AGENT',
     '使用方法': 'host:port/关键词'
 }
 
@@ -36,10 +38,29 @@ def index():
     return api_list
 
 
+@app.route('/delete/', methods=['GET'])
+def delete():
+    proxy = request.args.get('proxy')
+    ProxyManager().delete(proxy)
+    return 'success'
+
+
 @app.route('/get/')
 def get():
     proxy = ProxyManager().get()
     return proxy if proxy else 'no proxy!'
+
+
+@app.route('/get_all/')
+def getAll():
+    proxies = ProxyManager().getAll()
+    return proxies
+
+
+@app.route('/get_status/')
+def getStatus():
+    status = ProxyManager().getNumber()
+    return status
 
 
 @app.route('/refresh/')
@@ -49,23 +70,36 @@ def refresh():
     return 'success'
 
 
-@app.route('/get_all/')
-def getAll():
-    proxies = ProxyManager().getAll()
-    return proxies
-
-
-@app.route('/delete/', methods=['GET'])
-def delete():
-    proxy = request.args.get('proxy')
-    ProxyManager().delete(proxy)
-    return 'success'
-
-
-@app.route('/get_status/')
-def getStatus():
-    status = ProxyManager().getNumber()
-    return status
+@app.route('/user_agent/')
+def user_agent():
+    user_agent_list = [
+        # 谷歌浏览器
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+        # safari 5.1 – Windows
+        'User-Agent,Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'
+        # 搜狗浏览器 1.x
+        'User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+        # UC浏览器
+        'User-Agent,Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4094.1 Safari/537.36',
+        # 微软 Edge 浏览器
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586',
+        # 360浏览器
+        'User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+        # IE 9.0
+        'User-Agent,Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+        # Firefox 4.0.1 – Windows
+        'User-Agent,Mozilla/5.0 (Windows NT 6.1; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
+        # Opera 11.11 – Windows
+        'User-Agent,Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+        # 傲游（Maxthon）
+        'User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)',
+        # 腾讯TT
+        'User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)',
+        # 世界之窗（The World） 3.x
+        'User-Agent, Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)',
+    ]
+    user_agent_one = random.sample(user_agent_list, 1)[0]
+    return user_agent_one
 
 
 def run():
