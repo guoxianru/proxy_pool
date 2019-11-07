8c2ae16ce2baa5925fd37c13fd6d1799e1004f61
guoxianru <820685755@qq.com>
2019-11-07 11:34:55 +0800

    修改测试网址
    修改测试网址

--- Test/testWebRequest.py
+++ Test/testWebRequest.py
@@ -8,7 +8,7 @@ def testWebRequest():
     :return:
     """
     wr = WebRequest()
-    request_object = wr.get('https://www.siliconexpert.cn/zh-cn/login')
+    request_object = wr.get('https://www.baidu.com/')
     assert request_object.status_code == 200
 
 
